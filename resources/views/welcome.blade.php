96737bfc7638785a1347f0effe53e470802695dd
Nahid Hassan <mdnahidhassan01@gmail.com>
2024-09-06 15:35:11 +0600

    route name changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.home>
     <div class="d-flex align-items-center vh-100 gap-5">
-        <a class="btn btn-info btn-lg" href="form">Create Ticket</a>
+        <a class="btn btn-info btn-lg" href="create-ticket">Create Ticket</a>
         <a class="btn btn-info btn-lg" href="tickets">Manage Ticket</a>
     </div>
 </x-layouts.home>
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('form', TicketFormController::class);
+Route::resource('create-ticket', TicketFormController::class);
 Route::resource('tickets', TicketController::class);
 Route::get('ticket-image/{filename}', [ImageController::class, 'showImage'])->name('showImage')->middleware('throttle:60,1');
